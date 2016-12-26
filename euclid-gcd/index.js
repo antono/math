32efc0cdcb22de9c0d93f107cde114a58982e464
Antono Vasiljev <antono.vasiljev@gmail.com>
2016-12-26 10:27:06 +0300

    removed unneded spaces

--- euclid-gcd/index.js
+++ euclid-gcd/index.js
@@ -3,25 +3,25 @@ exports.gcd = gcd;
 function gcd(...args) {
   let [first, second, ...rest] = args;
   let result = gcdBinary(first, second);
-  
+
   for (const next of rest) {
     result = gcdBinary(result, next);
   }
-  
+
   return result
 }
 
 function gcdBinary(a, b) {
   const values = [a, b];
-  
+
   if (values.every(x => x === 0)) {
     throw new Error('Both arguments cannot be 0');
   }
-  
+
   if (values.some(x => x === 0)) {
     return values.find(x => x !== 0)
   }
-  
+
   while (b !== 0) {
     if (a > b) {
       a = a - b;
@@ -29,6 +29,6 @@ function gcdBinary(a, b) {
       b = b - a;
     }
   }
-  
+
   return a;
 }
